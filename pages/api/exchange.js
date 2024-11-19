5b4f6197afbaa5d6f83dadb22697ef0c2bc237a1
NirmalNemade <nirmalnemade712gmail.com>
2024-11-19 11:57:20 +0530

    prod

---
+++ pages/api/exchange.js
@@ -0,0 +1,12 @@
+const soap = require('soap');
+
+const wsdlUrl = 'https://win-3icse76s4gj.sanchar.in:444/EWS/Services.wsdl';
+
+soap.createClient(wsdlUrl, (err, client) => {
+  if (err) {
+    console.error('Error creating SOAP client:', err);
+    return;
+  }
+  console.log('SOAP Client created:', client);
+  // Use the client to call methods on the service
+});
